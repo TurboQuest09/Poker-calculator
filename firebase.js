27b3c17771146370f4828fc8099e751923ae82e3
TurboQuest09 <naveh.omer@gmail.com>
2025-04-27 20:19:22 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,21 @@
 // firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getDatabase, ref, set, push, onValue, remove, update } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
+import { initializeApp }  from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getDatabase, ref, set, push, onValue, remove, update }
+        from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
 
-// הגדרות Firebase שלך
+// הגדרות Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBDXDiVPlT9Xc4-OGo9soTxmREtMR1gFHg",
   authDomain: "poker-eacf2.firebaseapp.com",
   databaseURL: "https://poker-eacf2-default-rtdb.firebaseio.com",
   projectId: "poker-eacf2",
-  storageBucket: "poker-eacf2.firebasestorage.app",
+  storageBucket: "poker-eacf2.appspot.com",
   messagingSenderId: "1023833535660",
   appId: "1:1023833535660:web:94ae3e2e0b8193d3625b09"
 };
 
-// חיבור לאפליקציה
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+const db  = getDatabase(app);
 
-// ייצוא פעולות
+// מייצא את מה שצריך לשאר הקבצים
 export { db, ref, set, push, onValue, remove, update };
