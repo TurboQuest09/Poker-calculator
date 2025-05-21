e57d2e31225622e3d47f12a4329f1937e5cd8d4d
TurboQuest09 <naveh.omer@gmail.com>
2025-05-21 14:31:37 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -12,6 +12,13 @@ import {
   remove,
   onValue
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 /* ---  החלפתי כאן לפרטי-הפרויקט שסיפקת  --- */
 const firebaseConfig = {
@@ -19,14 +26,19 @@ const firebaseConfig = {
   authDomain:        "poker-eacf2.firebaseapp.com",
   databaseURL:       "https://poker-eacf2-default-rtdb.firebaseio.com",
   projectId:         "poker-eacf2",
-  storageBucket:     "poker-eacf2.firebasestorage.app",
+storageBucket: "poker-eacf2.appspot.com",
   messagingSenderId: "1023833535660",
   appId:             "1:1023833535660:web:94ae3e2e0b8193d3625b09"
 };
 
 /* --- אתחול Firebase --- */
-export const app = initializeApp(firebaseConfig);
-export const db  = getDatabase(app);
+export const app  = initializeApp(firebaseConfig);
+export const db   = getDatabase(app);
+export const auth = getAuth(app);
 
-/* --- מייצא פונקציות שתצטרך ב-app.js --- */
-export { ref, push, set, remove, onValue };
+/* --- ייצוא פונקציות לשימוש בקבצים אחרים --- */
+export {
+  ref, push, set, remove, onValue,                 // Realtime DB
+  createUserWithEmailAndPassword, signInWithEmailAndPassword,
+  signOut, onAuthStateChanged                      // Auth
+};
